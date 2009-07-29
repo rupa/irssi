5cce931e90fd2c5fca961d894a51ea772ab5889b
rupa <rupa@lrrr.us>
2009-07-28 22:50:19 -0400

    redundancy

--- defense.pl
+++ defense.pl
@@ -20,8 +20,7 @@ print CLIENTCRAP "defense.pl $VERSION loading.";
 my @defensechans = ("#mefi", "#dongs");
 
 sub frigth_back {
-    my ($data) = @_;
-    my ($tag, $cmd) = split(/\|/, $data);
+    my ($tag, $cmd) = split(/\|/, $@_);
     my $server = Irssi::server_find_tag($tag);
     $server->command($cmd);
 }
