d04b7945724b2f02631d61cd7a7a96ae5a302ce3
rupa <rupa@lrrr.us>
2009-07-29 00:03:05 -0400

    defense

--- defense.pl
+++ defense.pl
@@ -3,6 +3,8 @@ use strict;
 use vars qw($VERSION %IRSSI);
 
 $VERSION = "0.0.1";
+print CLIENTCRAP "defense.pl $VERSION loading.";
+print CLIENTCRAP "/set frigth_back [on/off] - toggle /kick your attacker.";
 %IRSSI = (
     authors     => "31d1",
     contact     => "",
@@ -14,10 +16,10 @@ $VERSION = "0.0.1";
     license     => "GPLv2",
 );
 
-print CLIENTCRAP "defense.pl $VERSION loading.";
-
 # chans to defend yourself in
 my @defensechans = ("#mefi", "#dongs");
+# give Chanserv some time to op us before we frigth_back, in ms
+my $hang = 2000;
 
 sub frigth_back {
     my ($tag, $cmd) = split(/\|/, @_);
@@ -30,20 +32,22 @@ sub onmode {
 
     return unless grep(/$chan/, @defensechans);
 
+    return unless $mode =~ /\Q$server->{nick}\E/;
+
     # get unbanned
-    if( $mode =~ /^\+b/ && $mode =~ /\Q$server->{nick}\E/ ) {
+    if( $mode =~ /^\+b/ ) {
         $server->command("/msg Chanserv unban $chan $server->{nick}");
-        Irssi::timeout_add_once(2000, "fk_u", "$server->{tag}|/join $chan");
+        Irssi::timeout_add_once($hang, "fk_u", "$server->{tag}|/join $chan");
 
         return if not Irssi::settings_get_bool("frigth_back");
-        Irssi::timeout_add_once(2000, "fk_u", "$server->{tag}|/kick $chan $setby");
+        Irssi::timeout_add_once($hang, "fk_u", "$server->{tag}|/kick $chan $setby");
 
     # regain ops
-    } elsif( $mode =~ /^\-o/ && $mode =~ /\Q$server->{nick}\E/ ) {
+    } elsif( $mode =~ /^\-o/ ) {
         $server->command("/msg Chanserv op $chan $server->{nick}");
 
         return if not Irssi::settings_get_bool("frigth_back");
-        Irssi::timeout_add_once(2000, "fk_u", "$server->{tag}|/kick $chan $setby");
+        Irssi::timeout_add_once($hang, "fk_u", "$server->{tag}|/kick $chan $setby");
     }
 
 }
