3bdcc498bffa2f77d180cd5b2a4d1bb8d4119968
rupa <rupa@lrrr.us>
2010-08-05 19:13:33 -0400

    kickjoin time and formatting

--- kickjoin.pl
+++ kickjoin.pl
@@ -19,27 +19,42 @@ my %kicklist;
 
 print CLIENTCRAP "kickjoin.pl $VERSION loaded.";
 print CLIENTCRAP "/set kickjoin on|off";
-print CLIENTCRAP "/kickjoin [-] [nick]";
+print CLIENTCRAP "/kickjoin [-] [nick] [seconds]";
 
 sub kicklist {
     my ($msg, $server, $chan) = @_;
     my @msg = split(/ /, $msg);
     my ($nick, $a);
+
     if( ! @msg ) {
         # list all
         print CLIENTCRAP Dumper(%kicklist);
+
     } elsif( $msg[0] eq "-" ) {
         # remove from list
         delete($kicklist{$msg[1]});
+
     } elsif( ! $chan ) {
         # list one
         print CLIENTCRAP Dumper($kicklist{$msg[0]});
+
     } else {
         # add to list
         $kicklist{$msg[0]} = [ $server->{tag}, $chan->{name}];
+
+        # remove from list at some point
+        if( $msg[1] ) {
+            my $say = "/MSG $chan->{name} eat shit for $msg[1] sec, $msg[0]";
+            $server->command($say);
+            Irssi::timeout_add_once($msg[1] * 1000, sub {
+                delete($kicklist{$msg[0]});
+                $server->command("/MSG $chan->{name} $msg[0] done eating shit");
+            }, []);
+            $server->command("/KICK $chan->{name} $msg[0]");
+        }
     }
 }
-    
+
 sub onjoin {
     return if not Irssi::settings_get_bool("kickjoin");
     my ($server, $chan, $nick, $addr) = @_;
