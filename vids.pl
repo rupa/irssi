045a4ce5c07d3d47c9e156ff553f4c32c75af74b
rupa <rupa@lrrr.us>
2011-05-13 20:29:42 -0400

    add flickr to vids.pl, and remove debugs, and try to prevent a fork bug

--- vids.pl
+++ vids.pl
@@ -9,8 +9,15 @@ use LWP::Simple;
 use HTML::Entities;
 use JSON;
 use Encode;
+use Net::Netrc;
 
 #use Data::Dumper;
+
+# flickr API key goes in ~/.netrc:
+# machine flickr.api
+#     login YOUR_API_KEY
+my $flickr_api = Net::Netrc->lookup('flickr.api');
+$flickr_api = $flickr_api->login if $flickr_api;
 
 use vars qw($VERSION %IRSSI);
 
@@ -27,8 +34,10 @@ print CLIENTCRAP "loading vids.pl $VERSION ...";
 
 sub youtube {
     my ($id) = @_;
-    print "youtube $id";
+    #print "youtube $id";
+    my %video = { service => "YOUTUBE", title => "", description => "" };
     my $content = get "http://gdata.youtube.com/feeds/api/videos/$id";
+    defined $content or return %video;
     my $ref = XMLin($content);
     my $title = $ref->{'title'}->{'content'};
     (my $description = $ref->{'content'}->{'content'}) =~ s/\s+/ /g;
@@ -44,8 +53,10 @@ sub youtube {
 
 sub vimeo {
     my ($id) = @_;
+    my %video = { service => "VIMEO", title => "", description => "" };
     print "vimeo $id";
     my $content = get "http://vimeo.com/api/v2/video/$id.json";
+    defined $content or return %video;
     my $text = from_json($content);
     #print Dumper($text);
     my %video = ( service => "VIMEO",
@@ -58,20 +69,48 @@ sub vimeo {
     return %video;
 }
 
+sub flickr {
+    my ($id) = @_;
+    my %photo = { service => "FLICKR", title => "", description => "" };
+    my $url = "http://api.flickr.com/services/rest/";
+    my $query = "method=flickr.photos.getInfo&api_key=$flickr_api&photo_id=$id";
+    my $content = get "$url?$query";
+    defined $content or return %photo;
+    my $ref = XMLin($content);
+    my $title = $ref->{'photo'}->{'title'};
+    my $description = $ref->{'photo'}->{'description'};
+    $title = "" if ref($title) eq "HASH";
+    $description = "" if ref($description) eq "HASH";
+    my %photo = ( service => "FLICKR",
+                  title => $title,
+                  description => $description || "" );
+    for my $key ( keys %photo ) {
+        $photo{$key} =~ s/<br>/ /g;
+        $photo{$key} =~ s/http:\/\///g;
+    }
+    return %photo;
+}
+
 sub parse_string {
     # take a string and return an array of hashes {service, title, description}
     # one per video
     my ($str) = @_;
-    print "parsing $str";
+    #print "parsing $str";
     my @words = split(/ /, $str);
     my @out = ();
     foreach (@words) {
-       next if $_ !~ /^http:\/\//;
-       if ( $_ =~ /http:\/\/(www\.)?youtube\.com\/watch\?v=([^\.\!\?,&]*)/ ) {
-           push(@out, {youtube($2)});
-       } elsif( $_ =~ /^http:\/\/(www\.)?vimeo\.com\/([^\.\!\?,]*)/ ) {
-           push(@out, {vimeo($2)});
-       }
+        next if $_ !~ /^http:\/\//;
+        if ( $_ =~ /http:\/\/(www\.)?youtube\.com\/watch\?v=([^\.\!\?,&]*)/ ) {
+            push(@out, {youtube($2)});
+        } elsif( $_ =~ /^http:\/\/(www\.)?vimeo\.com\/([^\.\!\?,]*)/ ) {
+            push(@out, {vimeo($2)});
+        } elsif( $flickr_api ) {
+            if( $_ =~ /http:\/\/(www\.)?flickr\.com\/photos\/[^\/]+\/([^\/]+)\// ) {
+                push(@out, {flickr($2)});
+            } elsif( $_ =~ /http:\/\/[^\.]+\.static\.flickr\.com\/[^\/]+\/([^_]+)_/ ) { 
+                push(@out, {flickr($1)});
+            }
+        }
     }
     return @out;
 }
